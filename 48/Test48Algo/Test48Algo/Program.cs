b07cdf225ce55ea475aba8682824bbe9a35845da
zavialovserge <zavialovserge@gmail.com>
2017-01-02 21:47:28 +0200

    Test№48

---
+++ 48/Test48Algo/Test48Algo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using System.Numerics; // специальная бибилитека для больших чисел
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test48Algo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger sum=0;
+            for (int i = 1; i <= 1000; i++)
+            {
+                sum += BigInteger.Pow(i, i);
+            }
+            string s = sum.ToString();
+            Console.WriteLine(s.Substring(s.Length-10,10));
+            Console.ReadKey();
+        }
+    }
+}
